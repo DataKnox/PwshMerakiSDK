49d5367f643b884824b43b734afbb8ad8f93d30a
Knox Hutchinson <knox@knoxsdata.com>
2020-02-06 20:20:46 -0600

    Get Client history cmdlets

--- holder.ps1
+++ holder.ps1
@@ -1,3 +1,5 @@
 $token = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+$net =Get-MerakiOrgs -Token $token | Where-Object { $_.name -eq "Devnet Sandbox" } | Get-MerakiNets -Token $token | Where-Object { $_.name -eq "DNSMB4-s.jayaraj007007@gmail.com" } 
 
 Get-MerakiOrgs -Token $token | Where-Object { $_.name -eq "Devnet Sandbox" } | Get-MerakiNets -Token $token | Where-Object { $_.name -eq "DNSMB3" } | Get-MerakiClients -Token $token
+
