99608c076b3e45cf6434fde016a724d1eaa4fafc
Knox Hutchinson <knox@knoxsdata.com>
2020-02-12 11:09:01 -0600

    Update Meraki SSIDs cmdlet

--- SetMerakiSSIDCmdlet.cs
+++ SetMerakiSSIDCmdlet.cs
@@ -9,9 +9,9 @@ using System.Collections.Generic;
 
 namespace GetMerakiOrgsCmdlet
 {
-    [Cmdlet(VerbsCommon.Get, "merakissids")]
-    [OutputType(typeof(MerakiSSID))]
-    public class GetMerakiSsidsCommand : PSCmdlet
+    [Cmdlet(VerbsCommon.Set, "MerakiSSID")]
+    [OutputType(typeof(ResponseSsid))]
+    public class SetMerakiSsidCommand : PSCmdlet
     {
         [Parameter(
             Mandatory = true,
@@ -27,27 +27,74 @@ namespace GetMerakiOrgsCmdlet
             ValueFromPipelineByPropertyName = true)]
         public string netid { get; set; }
 
-        private static async Task<IList<MerakiSSID>> GetSsids(string Token, string netid)
+        [Parameter(
+            Mandatory = true,
+            Position = 2,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string name { get; set; }
+
+        [ValidateSet("open", "psk", "open-with-radius", "8021x-meraki", "8021x-radius", IgnoreCase = true)]
+        [Parameter(
+            Mandatory = false,
+            Position = 3,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string authMode { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 4,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public int number { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            Position = 5,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string psk { get; set; }
+
+        [ValidateSet("WPA1 and WPA2", "WPA2 Only", IgnoreCase = true)]
+        [Parameter(
+            Mandatory = false,
+            Position = 6,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string wpaEncryptionMode { get; set; }
+
+        private static async Task<string> SetSsid(string Token, string netid, UpdateSsid ssid)
         {
             using (HttpClient client = new HttpClient())
             {
+                string numberval = ssid.number.ToString();
+                string jsonString;
+                string uri;
+                uri = $"https://dashboard.meraki.com/api/v0/networks/{netid}/ssids/{numberval}";
+                jsonString=JsonSerializer.Serialize<UpdateSsid>(ssid);
+                
+                var content = new StringContent(jsonString);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(
                     new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
                 client.DefaultRequestHeaders.Add("X-Cisco-Meraki-API-Key", Token);
 
-                var streamTask = client.GetStreamAsync($"https://dashboard.meraki.com/api/v0/networks/{netid}/ssids");
+                var response = await client.PutAsync(uri,content);
+                var contents = await response.Content.ReadAsStringAsync();
                 
-                return await JsonSerializer.DeserializeAsync<IList<MerakiSSID>>(await streamTask);
+                return contents;
             }
         }
 
-        private static  IList<MerakiSSID> ProcessRecordAsync(string Token, string netid)
+        private static string ProcessRecordAsync(string Token, string netid, UpdateSsid ssid)
         {
-            var task = GetSsids(Token, netid);
+            var task = SetSsid(Token, netid, ssid);
             task.Wait();
             var result = task.Result;
-            Console.WriteLine(result);
             return result;
         }
 
@@ -61,14 +108,24 @@ namespace GetMerakiOrgsCmdlet
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            WriteVerbose("Entering Get Orgs call");
-            var list = ProcessRecordAsync(Token, netid);
+            UpdateSsid ssid = new UpdateSsid();
+            ssid.name = name;
+            ssid.number = number;
+            ssid.enabled = true;
+            if(String.IsNullOrEmpty(authMode)){
+                ssid.authMode = authMode;
+            }
             
-            WriteObject(list,true);
 
+            WriteVerbose("Entering Get Orgs call");
+            var list = ProcessRecordAsync(Token, netid, ssid);
+
+            ResponseSsid result = JsonSerializer.Deserialize<ResponseSsid>(list);
+            WriteObject(result,true);
 
             WriteVerbose("Exiting foreach");
         }
+
 
         // // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
         protected override void EndProcessing()
@@ -77,13 +134,18 @@ namespace GetMerakiOrgsCmdlet
         }
     }
 
-    public class RadiusServer
+    public class UpdateSsid
     {
-        public string host { get; set; }
-        public int port { get; set; }
+        public string name { get; set; }
+        public bool enabled { get; set; }
+        public int number {get; set;}
+        public string authMode { get; set; }
+        public string psk { get; set; }
+        public string wpaEncryptionMode { get; set; }
     }
 
-    public class MerakiSSID
+
+    public class ResponseSsid
     {
         public int number { get; set; }
         public string name { get; set; }
