15d76c4354e31c0c8870416ade2c1b4fda95bf29
Knox Hutchinson <knox@knoxsdata.com>
2020-02-16 19:52:08 -0600

    Get Content Filter Categories cmdlet

---
+++ GetMxContentFilterCategories.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace GetMerakiOrgsCmdlet
+{
+    [Cmdlet(VerbsCommon.Get, "ContentFilterCategories")]
+    [OutputType(typeof(ContentFilterCat))]
+    public class GetContentFilterCatsCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string Token { get; set; }
+
+       [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string netid { get; set; }
+
+        private static async Task<ContentFilterCat> GetCFCats(string Token, string netid)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("X-Cisco-Meraki-API-Key", Token);
+
+                var streamTask = client.GetStreamAsync($"https://dashboard.meraki.com/api/v0/networks/{netid}/contentFiltering/categories");
+                
+                return await JsonSerializer.DeserializeAsync<ContentFilterCat>(await streamTask);
+            }
+        }
+
+        private static ContentFilterCat ProcessRecordAsync(string Token, string netid)
+        {
+            var task = GetCFCats(Token, netid);
+            task.Wait();
+            var result = task.Result;
+            return result;
+        }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+            WriteVerbose(Token);
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            WriteVerbose("Entering Get Orgs call");
+            var list = ProcessRecordAsync(Token, netid);
+            foreach(var item in list.categories)
+            {
+                WriteObject(item,true);
+            }
+            WriteVerbose("Exiting foreach");
+        }
+
+        // // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    } // end Get-MerakiOrgs
+
+    public class CFCategory
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class ContentFilterCat
+    {
+        public List<CFCategory> categories { get; set; }
+    }
+}
